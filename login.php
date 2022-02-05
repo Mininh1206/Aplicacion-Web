3010a6242e41632dbd13316899e1f3732bd3d94a
Daniel Marchena <danielmarchenajimenez2002@gmail.com>
2022-02-05 20:32:53 +0100

    Primera version

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include "inc/head.html";
+    ?>
+    <title>Log in</title>
+</head>
+
+<body>
+    <?php
+    include "inc/menu.html";
+    ?>
+
+    
+
+    <script src="js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+</body>
+
+</html>
---
+++ signin.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php
+    include "inc/head.html";
+    ?>
+    <title>Sing in</title>
+</head>
+
+<body>
+    <?php
+    include "inc/menu.html";
+    ?>
+
+    <script src="js/vendor/jquery-2.1.4.min.js"></script>
+    <script src="js/vendor/foundation.js"></script>
+    <script>
+        $(document).foundation();
+    </script>
+</body>
+
+</html>
